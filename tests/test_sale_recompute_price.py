19df59ad104b35bda91371fd81d087d944b298ac
??ngel ??lvarez <angel@nan-tic.com>
2016-05-12 19:15:51 +0200

    fix scenario

--- tests/test_sale_recompute_price.py
+++ tests/test_sale_recompute_price.py
@@ -3,23 +3,14 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import (doctest_setup, doctest_teardown,
+    doctest_checker)
 
 
-class TestSaleRecomputePriceCase(unittest.TestCase):
+class TestSaleRecomputePriceCase(ModuleTestCase):
     'Test Sale Recompute Price module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_recompute_price')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_recompute_price')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_recompute_price'
 
 
 def suite():
@@ -28,5 +19,6 @@ def suite():
             TestSaleRecomputePriceCase))
     suite.addTests(doctest.DocFileSuite('scenario_sale_recompute_price.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+            checker=doctest_checker))
     return suite
